4a25ba05e2e5840f1b8e14222b5f5ea97b3d6dc2
mauro.lavin <villarroelmauroemanuel@gmail.com>
2022-08-02 23:55:37 -0300

    entrega optimizacion codigo. mejore el codigo, agregue millas al conversor y optimice la vista responsive.

--- index.js
+++ index.js
@@ -20,48 +20,84 @@ function conversion(){
 
     if(unit1.value=="kilometer" && unit2.value=="kilometer"){
         result = number * 1;
-        conversionResult.innerHTML = number + " Kilometro/s equivalen a " + result + " Kilometro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="kilometer" && unit2.value=="meter"){
         result = number * 1000;
-        conversionResult.innerHTML = number + " Kilometro/s equivalen a " + result + " Metro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="kilometer" && unit2.value=="centimeter"){
         result = number * 10000;
-        conversionResult.innerHTML = number + " Kilometro/s equivalen a " + result + " Centimetro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="centimeter" && unit2.value=="centimeter"){
         result = number * 1;
-        conversionResult.innerHTML = number + " Centimetro/s equivalen a " + result + " Centimetro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="centimeter" && unit2.value=="meter"){
         result = number / 100;
-        conversionResult.innerHTML = number + " Centimetro/s equivalen a " + result + " Metro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="centimeter" && unit2.value=="kilometer"){
         result = number / 1000;
-        conversionResult.innerHTML = number + " Centimetro/s equivalen a " + result + " Kilometro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="meter" && unit2.value=="meter"){
         result = number * 1;
-        conversionResult.innerHTML = number + " Metro/s equivalen a " + result + " Metro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
     
     if(unit1.value=="meter" && unit2.value=="centimeter"){
         result = number * 100;
-        conversionResult.innerHTML = number + " Metro/s equivalen a " + result + " Centimetro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
 
     if(unit1.value=="meter" && unit2.value=="kilometer"){
         result = number / 1000;
-        conversionResult.innerHTML = number + " Metro/s equivalen a " + result + " Kilometro/s ";
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="miles"){
+        result = number * 1;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="kilometer"){
+        result = number * 1.60934;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="centimeter"){
+        result = number / 0.0000062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="miles" && unit2.value=="meter"){
+        result = number / 0.00062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+
+    if(unit1.value=="meter" && unit2.value=="miles"){
+        result = number * 0.00062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
     }
     
-
+    if(unit1.value=="centimeter" && unit2.value=="miles"){
+        result = number * 0.0000062137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
+    
+    if(unit1.value=="kilometer" && unit2.value=="miles"){
+        result = number / 0.62137;
+        conversionResult.innerHTML = number + ' ' + unit1.value + " equivalen a " + result + " " + unit2.value;
+    }
 }
+
+
+
